63d1e2d39d79ff05daa6c959c46342b22a3627d1
gokulnathpeace@gmail.com <gokulnathpeace@gmail.com>
2024-10-12 03:01:12 +0530

    Landing page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
+import { Button } from '@/components/ui/Button'
+import { ArrowUpRight } from 'lucide-react'
+import Image from 'next/image'
+
+import AsterickBig from '../public/assets/Asterisk-left.svg'
+import AsterickSmall from '../public/assets/Asterisk-front.svg'
+import DiamondSmall from '../public/assets/Diamond-right.svg'
+import DiamondBig from '../public/assets/Diamond-left.svg'
 
 export default function Home() {
-
   return (
-    <div className="p-4">
-    </div>
-  );
+    <main className='flex flex-col gap-10 items-center h-[80vh] max-lg:h-[40vh] relative py-20 max-lg:py-10'>
+      <div className='flex flex-col items-center w-full'>
+        <h1 className='text-4xl font-powerGrotesk '>
+          Your One-Stop Event Solution.
+        </h1>
+        <p className='text-sm font-light tracking-widest'>
+          Streamline Your Event Journey.
+        </p>
+      </div>
+      <Button>
+        Create Event <ArrowUpRight />
+      </Button>
+
+      {/* Images */}
+      <Image
+        src={AsterickBig}
+        alt='Asterick Big'
+        className='z-[2] absolute right-[-40px] bottom-[-60px]'
+      />
+      <Image
+        src={AsterickSmall}
+        alt='Asterick Big'
+        className='z-[-10] absolute left-0 top-[-20px]'
+      />
+      <Image
+        src={DiamondBig}
+        alt='Asterick Big'
+        className='z-[-10] absolute left-0 bottom-0'
+      />
+      <Image
+        src={DiamondSmall}
+        alt='Asterick Big'
+        className='z-[-10] absolute right-0 top-0'
+      />
+    </main>
+  )
 }
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,21 +1,20 @@
 import Image from 'next/image'
 import React from 'react'
-import logo from "../../public/assets/XORA.png"
+import logo from '../../public/assets/XORA.png'
 import { Button } from '../ui/Button'
 import { ArrowUpRight } from 'lucide-react'
-import { Gradient } from '../ui/Gradient'
-
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
     <header className='w-full min-h-[100px]  flex justify-between items-center'>
+      <Image src={logo} height={20} alt='logo' />
+      <Button variant='text' className='items-end'>
+        Explore Events <ArrowUpRight />
+      </Button>
 
-        <Image src={logo}  height={20} alt="logo" />
-        <Button variant='text' className='items-end'>Explore Events <ArrowUpRight /></Button>
-
-        <div className='blur-[250px]  bg-gradient-to-t from-action to-[#759204] absolute top-[-160px] left-0 right-0 mx-auto rounded-full h-[335px] w-[335px]' />
+      <div className='blur-[250px]  bg-gradient-to-t from-action to-[#759204] absolute top-[-160px] left-0 right-0 mx-auto rounded-full h-[335px] w-[335px]' />
     </header>
   )
 }
